bef8c31cb572713b87f3633d4438210967e0cb0f
gityashh <yashrajput8359@gmail.com>
2024-11-21 15:09:02 +0530

    moving

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import React from "react";
+import { BsFacebook } from "react-icons/bs";
+import { FaFacebook, FaTwitter, FaPinterest, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      {/* Newsletter Subscription Section */}
+      <div className="newsletter">
+        <h3>Subscribe to our Newsletter</h3>
+        <p>Stay updated with our latest news and offers.</p>
+        <form className="newsletter-form">
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="newsletter-input"
+            required
+          />
+          <button type="submit" className="newsletter-button">
+            Subscribe
+          </button>
+        </form>
+      </div>
 
-export default Footer
+      <div className="footer-container">
+        
+        {/* Contact Us Section */}
+        <div className="footer-section">
+          <h4>Contact Us</h4>
+          <p>Demo Store</p>
+          <p>No. 1259 Freedom, New York, 11111</p>
+          <p>United States</p>
+          <p>+91-987654321</p>
+          <p>Demo@exampledemo.com</p>
+          <div className="footer-icons">
+            <a className="text-white fs-2" href="#">< FaFacebook /></a>
+            <a className="text-white fs-2" href="#">< FaTwitter /></a>
+            <a className="text-white fs-2" href="#">< FaPinterest /></a>
+            <a className="text-white fs-2" href="#">< FaInstagram /></a>
+          </div>
+        </div>
+
+        {/* Information Section */}
+        <div className="footer-section">
+          <h4>Information</h4>
+          <ul>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Refund Policy</a></li>
+            <li><a href="#">Shipping Policy</a></li>
+            <li><a href="#">Terms of Service</a></li>
+            <li><a href="#">Blogs</a></li>
+          </ul>
+        </div>
+
+        {/* Account Section */}
+        <div className="footer-section">
+          <h4>Account</h4>
+          <ul>
+            <li><a href="#">Search</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Size Chart</a></li>
+          </ul>
+        </div>
+
+        {/* Quick Links Section */}
+        <div className="footer-section">
+          <h4>Quick Links</h4>
+          <ul>
+            <li><a href="#">Accessories</a></li>
+            <li><a href="#">Laptops</a></li>
+            <li><a href="#">Headphones</a></li>
+            <li><a href="#">Smart Watches</a></li>
+            <li><a href="#">Tablets</a></li>
+          </ul>
+        </div>
+
+        {/* Our App Section */}
+        <div className="footer-section">
+          <h4>Our App</h4>
+          <p>Download our App and get extra 15% Discount on your first Order..!</p>
+          <div className="app-links">
+            <a href="#"><img src="https://play.google/howplayworks/static/assets/social/share_google_play_logo.png" alt="Google Play" /></a>
+            <a href="#"><img src="path-to-icon/app-store.png" alt="App Store" /></a>
+          </div>
+        </div>
+      </div>
+
+      {/* Bottom Bar */}
+      <div className="footer-bottom">
+        <p>© 2022, Digitic Powered by Shopify</p>
+        <div className="payment-icons">
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BsSearch } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -26,7 +26,7 @@ const Header = () => {
       </header>
       <header className="header-upper py-3">
         <div className="container-xxl">
-          <div className="row align-items-center">
+          <div className="row align-items-center ">
             <div className="col-2">
               <h2>
                 <Link className="text-white">DevCorner</Link>
@@ -49,28 +49,92 @@ const Header = () => {
             <div className="col-5">
               <div className="header-upper-links d-flex align-items-center justify-content-between">
                 <div>
-                  <Link to="/">
-                    <img src="" alt="" />
-                    <p></p>
+                  <Link
+                    to="/"
+                    className="d-flex align-items-center text-white gap-10"
+                  >
+                    <img src="/images/wishlist.svg" alt="wishlist" />
+                    <p className="mb-0">
+                      Favourite <br /> wishlist
+                    </p>
                   </Link>
                 </div>
                 <div>
-                  <Link to="/">
-                    <img src="" alt="" />
-                    <p></p>
+                  <Link
+                    to="/"
+                    className="d-flex align-items-center text-white gap-10"
+                  >
+                    <img src="/images/user.svg" alt="wishlist" />
+                    <p className="mb-0">
+                      Login <br /> My Account
+                    </p>
                   </Link>
                 </div>
-                <div>
-                  <Link to="/">
-                    <img src="" alt="" />
-                    <p></p>
+                <div className="align-items-cente ">
+                  <Link
+                    className="d-flex align-items-center text-white gap-10 mb-0"
+                    to="/"
+                  >
+                    <img src="images/cart.svg" alt="cart" />
+                    <div className="d-flex flex-column gap-1 mb-0">
+                      <span className="badge bg-white text-dark">0</span>
+                      <p className="mb-0">$ 500</p>
+                    </div>
                   </Link>
                 </div>
-                <div>
-                  <Link to="/">
-                    <img src="" alt="" />
-                    <p></p>
-                  </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-bottom">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-bottom d-flex align-items-center justify-content-center gap-30">
+                <div> 
+                  <div className="dropdown ">
+                    <button
+                      className="btn btn-secondary dropdown-toggle bg-transparent  border-0 gap-15 d-flex align-items-center  "
+                      type="button"
+                      id="dropdownMenuButton1"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <img src="/images/menu.svg" alt="menu" />
+                      <span className="me-5 d-inline-block">
+                        Shop Categories
+                      </span>
+                    </button>
+                    <ul
+                      className="dropdown-menu"
+                      aria-labelledby="dropdownMenuButton1"
+                    >
+                      <li>
+                        <Link className="dropdown-item text-white" to="/">
+                          Action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" to="/">
+                          Another action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" to="/">
+                          Something else here
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="menu-links">
+                  <div className="d-flex align-items-center gap-15">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/our-store">Our Store</NavLink>
+                    <NavLink to="/blogs">Blogs</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                  </div>
                 </div>
               </div>
             </div>
