cdadae709c23902d5d9a819fd397b91874cfb6e0
gityashh <yashrajput8359@gmail.com>
2024-11-23 15:24:57 +0530

    blog done

--- src/features/blogs/blogSlice.jsx
+++ src/features/blogs/blogSlice.jsx
@@ -1,13 +1,23 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { blogService } from "./blogService";
 
+
 const initialState = {
   blogs: [],
   isLoading: false,
   isError: false,
   isSuccess: false,
-  message: "",
+    message: "",
+  currentBlog: null,
 };
+
+export const getBlog = createAsyncThunk("blog/getBlog", async (id, thunkAPI) => {
+  try {
+    return await blogService.getBlog(id);
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response);
+  }
+});
 
 export const getBlogs = createAsyncThunk("blog/getBlogs", async (thunkAPI) => {
   try {
@@ -35,7 +45,20 @@ export const blogSlice = createSlice({
         state.isLoading = false;
         state.isError = true;
         state.message = action.error;
-      });
+      })
+      .addCase(getBlog.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getBlog.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.currentBlog = action.payload;
+      })
+      .addCase(getBlog.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.error;
+      })
   },
 });
 
